123f67e26db18c87caac50c5dd145b4c0409e0a5
calher99 <calher99@gmail.com>
2023-01-02 16:35:34 +0000

    finish styling and html for form components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses/>
       
     </div>
