43107ea148dea1cef012488d38d462e05fd016cb
calher99 <calher99@gmail.com>
2023-01-03 16:32:19 +0000

    allocate dummy array in App.js and create state for it

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
-import React , { useState } from 'react';
+// import React , { useState } from 'react';
 
 const ExpenseItem = (props) => {
   // useState(props.title) only works for the first time
   // Following calls of the function will fetch the value that was passed to setTitle
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () =>{
-    // Here we make React reevaluat the ExpenseItm function but title= Updated!
-    setTitle('Updated!');
-  };
+  // const clickHandler = () =>{
+  //   // Here we make React reevaluat the ExpenseItm function but title= Updated!
+  //   setTitle('Updated!');
+  // };
 
   return (
     // Pass className as props to get the class
@@ -19,10 +19,11 @@ const ExpenseItem = (props) => {
     <Card className="expense-item"> 
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        {/* <h2>{title}</h2> */}
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button> 
+      {/* <button onClick={clickHandler}>Change title</button>  */}
     </Card>
 
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,36 +2,23 @@ import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
-const Expenses = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ]
+const Expenses = (props) => {
+    const [selectedYear, setSelectedYear] = useState('2023');
+
+    const expensesFilterChangeHandler = (yearFilter) => {
+        setSelectedYear(yearFilter)
+        console.log(selectedYear)
+    }
+    
+
     return (
         <Card className='expenses'>
-            <ExpensesFilter/>
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+            <ExpensesFilter selected={selectedYear} onExpensesFilterChange={expensesFilterChangeHandler} />
+            {props.items.map(expense =>{
+                return (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)
+            })}
         </Card>
     );
 }
