44aeb211974b9777599167cfb702531798f74693
calher99 <calher99@gmail.com>
2023-01-03 13:06:48 +0000

    add expensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = () => {
     const expenses = [
@@ -26,6 +27,7 @@ const Expenses = () => {
     ]
     return (
         <Card className='expenses'>
+            <ExpensesFilter/>
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
