e38f3b5c32c34f35bdc61c9965efd3adfedb5813
calher99 <calher99@gmail.com>
2023-01-04 11:35:28 +0000

    create new array with filtered by year expenses in Expense.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,37 @@
-import './Expenses.css'
+import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
 
 const Expenses = (props) => {
-    const [selectedYear, setSelectedYear] = useState('2023');
+
+    const [selectedYear, setSelectedYear] = useState('2020');
 
     const expensesFilterChangeHandler = (yearFilter) => {
-        console.log(yearFilter)
         setSelectedYear(yearFilter)
-        console.log(selectedYear)
     }
-    
+    const itemsFiltered = props.items.filter(item => item.date.getFullYear() === parseInt(selectedYear));
 
+    // create a variable to store a logic outcome and print it in the component
+    let expensesContent = <p>No expenses found.</p>
+    if(itemsFiltered.length > 0 ){
+        expensesContent = itemsFiltered.map(expense =>{
+            return (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+            />)
+        })
+    }
     return (
         <Card className='expenses'>
             <ExpensesFilter 
             selected={selectedYear} 
             onExpensesFilterChange={expensesFilterChangeHandler} />
-
-            {props.items.map(expense =>{
-                return (
-                <ExpenseItem 
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date}
-                />)
-            })}
+            {expensesContent}
         </Card>
     );
 }
